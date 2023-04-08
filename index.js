f1b382bf5348960ac1263c501785d6ad9516d45c
Jagroshan <jagroshansingh615@gmail.com>
2023-04-08 12:52:01 +0530

    added .env for PORT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer=require('json-server')
 const cors=require('cors')
 const path=require('path')
+require('dotenv').config()
 
 const server=jsonServer.create()
 const router=jsonServer.router(path.join(__dirname,'db.json'))
@@ -11,6 +12,6 @@ server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
 
-server.listen(8888,()=>{
+server.listen(process.env.PORT,()=>{
     console.log('Server is running at 8888')
 })
