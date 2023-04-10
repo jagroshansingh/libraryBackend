e56ecaef50e9894166d339e39650be75bc041ca3
Jagroshan <jagroshansingh615@gmail.com>
2023-04-10 10:24:57 +0530

    json server backend is at place

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const jsonServer=require('json-server')
 const cors=require('cors')
-const path=require('path')
 require('dotenv').config()
+// const http=require('http')
+// console.log(http)
 
 const server=jsonServer.create()
-const router=jsonServer.router(path.join(__dirname,'db.json'))
+const router=jsonServer.router('db.json')
 const middlewares=jsonServer.defaults()
 
 server.use(cors())
